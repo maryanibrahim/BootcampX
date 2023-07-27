a5bab0477f1faaa33ac677bf411ca498461ba10a
maryanibrahim <maryanibrahim301@yahoo.com>
2023-07-27 05:31:18 +0000

    students that haven't added their Github username

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -1,4 +1,11 @@
-SELECT id, name, email, cohort_id
-FROM students
-WHERE github IS NULL
-ORDER BY cohort_id
+SELECT
+    id,
+    name,
+    email,
+    cohort_id
+FROM
+    students
+WHERE
+    github IS NULL
+ORDER BY
+    cohort_id;
